fc9fe51aac0b4c9815b459d537086b437c8c7260
“kidan16” <eyueltekle@hotmail.com>
2022-05-31 21:19:11 +0300

    random number

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if import == 0:
+    printf(f"{import} is zero")
+elif import > 0:
+    printf(f"{import} is positive")
+else import < 0:
+    printf(f"{import} is negative")
