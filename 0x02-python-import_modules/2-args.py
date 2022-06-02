bfeb53e02f5935b80ceac0c9b6e1037e1a735767
“kidan16” <eyueltekle@hotmail.com>
2022-06-02 23:45:14 +0300

    modulus

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,5 +7,5 @@ else:
 if __name__ == "__main__":
     print("{} arguments{}".format(len(argv), separetor))
     if len(argv) > 1:
-        for i in range(1, len(argv) + 1):
-            print("{}: {}".format(i, argv[i]))
+       for i in range(1, len(argv)):
+            print('{}: {}'.format(i, argv[i]))
