1bcec30a4d150908b91792542a9fe0115878dbd3
“kidan16” <eyueltekle@hotmail.com>
2022-06-02 23:53:36 +0300

    modulus

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 from sys import argv
-if len(argv) == 1:
-    separetor = "."
-else:
-    separetor = ":"
 if __name__ == "__main__":
-    print("{} arguments{}".format(len(argv), separetor))
+    if len(argv) == 1:
+        separetor = "."
+    else:
+        separetor = ":"
+    print("{} arguments{}".format(len(argv) - 1, separetor))
     if len(argv) > 1:
        for i in range(1, len(argv)):
             print('{}: {}'.format(i, argv[i]))
