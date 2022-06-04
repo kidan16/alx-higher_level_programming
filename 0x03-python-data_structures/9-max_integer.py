6bc629d4e62a69bc14b1c1f2b63d06bc4a099d98
“kidan16” <eyueltekle@hotmail.com>
2022-06-04 17:44:34 +0300

    correction

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None:
+    if my_list == []:
         return None
     for element in my_list:
-        Max = 0
+        Max = my_list[0]
         if element > Max:
             Max = element
     return Max
