d9a6698e3e9fc50270179d9b52079c2d300902a3
“kidan16” <eyueltekle@hotmail.com>
2022-06-04 17:22:56 +0300

    correction

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
-        return
     for List in matrix:
         for element in List:
             if element == List[len(List) - 1]:
