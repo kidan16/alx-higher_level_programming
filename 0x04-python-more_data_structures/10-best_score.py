c1bdbd0a91a0091a7acadffb6f3b7d6e05ae8128
Kidan16 <eyueltekle@gmail.com>
2022-06-08 21:46:39 +0300

    task -04

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if type(a_dictionary) != dict or a_dictionary == {}:
         return None
     key_list = list(a_dictionary.keys())
-    _max = 0
+    _max = a_dictionary[key_list[0]]
     for key_word in key_list:
          if a_dictionary[key_word] > _max:
              _max = a_dictionary[key_word]
