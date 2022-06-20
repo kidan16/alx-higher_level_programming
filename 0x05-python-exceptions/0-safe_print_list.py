31d194ec587e5ac95967dbda486ec02feeaa8fa3
kidan16 <eyueltekle@hotmail.com>
2022-06-20 10:30:24 +0300

    0-safe_print_list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    for y in range(x):
+        try:
+            print(my_list[y], end="")
+        except IndexError:
+            print("")
+            return y
+    print("")
+    return y + 1
