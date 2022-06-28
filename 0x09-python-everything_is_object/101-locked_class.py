786d55954fb2a88757ca7d16081d105cd36abcab
kidan16 <eyueltekle@hotmail.com>
2022-06-28 22:51:08 +0300

    0x09-python-everything_is_object

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,14 +6,10 @@ LockedClass
 
 class LockedClass:
     """ No class or object attributes, can't set
-    Except for first_name
+        Except for first_name
     """
-
     def __setattr__(self, attribute, value):
-
-        word = "'LockedClass' object has no attribute '"
-
         if attribute == "first_name":
             self.__dict__[attribute] = value
         else:
-            raise AttributeError(word + attribute + "'")
+            raise AttributeError("'LockedClass' object has no attribute '" + attribute + "'")
