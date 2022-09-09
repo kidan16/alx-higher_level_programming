214193a8eb461582c3b3f0084dca4fd93c1ee079
Kidan16 <eyueltekle@hotmail.com>
2022-09-09 21:54:36 +0300

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-'use strict';
-let arg = process.argv.length;
-if (arg === 2) {
-	  console.log('No argument');
-} else if (arg === 3) {
+
+if (process.argv.length > 3) {
+	  console.log('Arguments found');
+} else if (process.argv.length === 3) {
 	  console.log('Argument found');
 } else {
-	  console.log('Arguments found');
+	  console.log('No argument');
 }
